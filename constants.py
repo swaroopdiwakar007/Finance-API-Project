fb7357d754bd632370cf29c53579b5157011e110
SPDiw <swaroopdiwakar007@gmail.com>
2024-01-03 17:29:55 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+# URL = "https://finance.yahoo.com/quote/nse_stock_symbol.NS?p=nse_stock_symbol.NS"
+URL = "https://finance.yahoo.com/quote/nse_stock_symbol.stock_exchange/"
+URL2 = "https://finance.yahoo.com/quote/nse_stock_symbol.stock_exchange/history"
+
+MAX_COUNT = 20
+DICT1 = {
+    "PREV VAL":"",
+    "OPEN VAL":"",
+    "CURR VAL":"",
+    "BETA VAL":"",
+    "PE RATIO":"",
+    "EPS RATIO":"",
+}
+
+STOCK_EXCHANGE = 'NS'
+
+SECONDS_IN_A_YEAR = 31556952
+
+ONE_DAY = '1d'
+HISTORY = 'history'
+TRUE_VAL = 'true'
+
